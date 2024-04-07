eb47f2e1e0a6535bb1c178c7fbfefb0d7664a710
Akshit jain <113924385+akshitjain16@users.noreply.github.com>
2024-04-08 00:12:31 +0530

    Time: 3 ms (45.50%) | Memory: 7.4 MB (53.16%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,13 +1,31 @@
 class Solution {
 public:
     bool checkValidString(string s) {
-        int low = 0, high = 0;
-        for (char c : s) {
-            low += (c == '(') ? 1 : -1;
-            high += (c != ')') ? 1 : -1;
-            if (high < 0) return false;
-            low = max(low, 0);
+        int open = 0;
+        int close = 0;
+        // every closing bracket there exist an opening bracket
+        // O(n)
+        for (int index = 0; index < s.size(); index++) {
+            if (s[index] == '(' || s[index] == '*')
+                open++;
+            else {
+                if (open == 0)
+                    return false;
+                else
+                    open--;
+            }
         }
-        return low == 0;
-    }
-};
+        // every opening bracket these exist a closing brcaket
+        for (int index = s.size() - 1; index >= 0; index--) {
+            if (s[index] == ')' || s[index] == '*')
+                close++;
+            else {
+                if (close == 0)
+                    return false;
+                else
+                    close--;
+            }
+        }
+        return true;
+        }
+    };
